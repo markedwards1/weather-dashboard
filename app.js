00f021707466c6c6707e37da13f01792506ae7a7
Mark Edwards <markedwards@Marks-MBP.lan>
2022-06-27 22:37:13 +0800

    new approach

--- app.js
+++ app.js
@@ -25,65 +25,73 @@
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
 const cityResults = document.getElementById('search-container');
-
+const currentTemp = document.getElementById('current-temp');
+const currentWind = document.getElementById("current-wind");
+const currentHumidity = document.getElementById("current-humidity");
 searchBtn.addEventListener('click', search);
 
 
 const attriNum = 0
-getHistory();
+//getHistory();
 
 
 //search geo location api with  users city input 
-function getAPIcityLatLong (){
-    const requestUrlCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput.value + "&limit=5&appid=75da345c5502ba66bc2eb539c0c3e2f0";
+function getAPIcityLatLong(){
+    //const requestUrlCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput.value + "&limit=5&appid=75da345c5502ba66bc2eb539c0c3e2f0";
+   // let requestUrlCity = "https://api.openweathermap.org/data/2.5/weather?q=“ + "perth" + ”&units=metric” + "&appid=" + "75da345c5502ba66bc2eb539c0c3e2f0";
+    const requestUrlCity = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&units=metric&appid=75da345c5502ba66bc2eb539c0c3e2f0"
+
     fetch(requestUrlCity)
     .then(function (responseCity){
         return responseCity.json();
     })
     .then(function (cityData){
-        //console.log(cityData);
-        const currentCityLat = cityData.lat;
-        const currentCityLon = cityData.lon;
-        console.log(cityData.name.lat);
+        console.log(cityData);
+        console.log(cityData.main.temp);
+        console.log(cityData.wind.speed);
+        currentTemp.textContent = "Temp: " + cityData.main.temp + "C";
+        currentWind.textContent = "Wind: " + cityData.wind.speed + "C";
+        currentHumidity.textContent = "Humidity: " + cityData.main.humidity + "C";
+        //uv index https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
 
 
-        // separate cityData in to 5 separate arrays.    
-        for (let index = 0; index < cityData.length; index++) {
-            // const element = cityData[index];
-            console.log(cityData[index].name);
-            console.log(cityData[index].country);                 
-                const city = document.createElement('button');
+           
+        // for (let index = 0; index < cityData.length; index++) {
+        //     // const element = cityData[index];
+        //     console.log(cityData[index].name);
+        //     console.log(cityData[index].country);                 
+        //         const city = document.createElement('button');
             
 
-              ;
-                city.setAttribute('id', "cityButton" + index);                                
-                //Print out text content for city & country
-                city.textContent = cityData[index].name + " " + cityData[index].state + " " + cityData[index].country;                
+        //       ;
+        //         city.setAttribute('id', "cityButton" + index);                                
+        //         //Print out text content for city & country
+        //         city.textContent = cityData[index].name + " " + cityData[index].state + " " + cityData[index].country;                
                 
 
 
-                // append child element search - container
-                cityResults.appendChild(city);         
-                let city1 = document.getElementById('cityButton' + index);
-                city1.addEventListener('click', saveCity);
-                function saveCity(){
-                    city1 = city1.textContent;
-                    console.log("savecity()" + city1);
-                    // location.reload();
+        //         // append child element search - container
+        //         cityResults.appendChild(city);         
+        //         let city1 = document.getElementById('cityButton' + index);
+        //         city1.addEventListener('click', saveCity);
+        //         function saveCity(){
+        //             city1 = city1.textContent;
+        //             console.log("savecity()" + city1);
+        //             // location.reload();
 
-                    //set local storage
-                    let storedCity = localStorage.setItem(index, city1);
-                    cityDisplay();
-                    //get local storage
+        //             //set local storage
+        //             let storedCity = localStorage.setItem(index, city1);
+        //             cityDisplay();
+        //             //get local storage
                
-                }
+        //         }
 
-                function cityDisplay(){
-                    let displaycity = localStorage.getItem(index);
-                    //displaycity = displaycity.textContent;
-                    console.log("getItem " + displaycity);
-                    const citySide1 = document.getElementById('city-side-1');
-                    citySide1.textContent = (displaycity);
+                // function cityDisplay(){
+                //     let displaycity = localStorage.getItem(index);
+                //     //displaycity = displaycity.textContent;
+                //     console.log("getItem " + displaycity);
+                //     const citySide1 = document.getElementById('city-side-1');
+                //     citySide1.textContent = (displaycity);
 
                     //render search history into 
                       
@@ -94,7 +102,7 @@ function getAPIcityLatLong (){
 
 
 
-                  }
+                  })
         }
         //get lat & long
         //add eventListener to save city to history. 
@@ -103,21 +111,21 @@ function getAPIcityLatLong (){
         
         
         
-    })
+   // })
     
     
-} // end of getAPIcityLatLong
+//} // end of getAPIcityLatLong
 
 
-function getHistory(){
-    const getHistory = localStorage.getItem(0);
-    console.log('outside ' + getHistory)
+// function getHistory(){
+//     const getHistory = localStorage.getItem(0);
+//     console.log('outside ' + getHistory)
 
-}
+// }
 
 
 
-//grab users city input from search
+// //grab users city input from search
 
 
 
@@ -127,5 +135,4 @@ function search(){
     getAPIcityLatLong()
 
 
-
-}
+    }
